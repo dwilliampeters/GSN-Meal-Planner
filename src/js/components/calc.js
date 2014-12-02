198bbc7ce5f444e612496b94c27093706d3d418b
danp <spartdan@live.co.uk>
2014-12-02 22:33:51 +0000

    step 2 and step 4

--- src/js/components/calc.js
+++ src/js/components/calc.js
@@ -9,7 +9,7 @@
     var
       calcId              = calcId,
       calcVal             = calcVal,
-      sumVal,
+      sumVal              = 0,
       // Step 1
       gender              = $('[data-calc="gender"]:checked').val(),
       age                 = parseInt($('[data-calc="age"]').val()),
@@ -19,48 +19,47 @@
       formula             = parseInt($('[data-calc="formula"]:checked').val()),
       activity            = parseFloat($('select[data-calc="activity"] option:selected').val()),
       activityDesc        = $('select[data-calc="activity"] option:selected').text(),
-      /*workCarbs           = $('[data-calc-work="carbs"]').val(),
-      workProt            = $('[data-calc-work="prot"]').val(),
-      workFat             = $('[data-calc-work="fat"]').val(),
-      workWeek1           = $('[data-calc-work="week1"]').val(),
-      workWeek2           = $('[data-calc-work="week2"]').val(),
-      workWeek3           = $('[data-calc-work="week3"]').val(),
-      workWeek4           = $('[data-calc-work="week4"]').val(),*/
+      // Step 2
+      goal,
+      $goal               = $('[data-calc-goal]'),
+      goalSelected        = parseInt($('[data-calc-goal]:checked').val()),
+      goalCalsSelected    = 0,
+      goalVal             = 0,
+      $goalCals           = $('[data-calc-goal-calories]'),
+      goalCals            = 0,
       // BMR
       BMR1                = parseInt($('[data-calc-bmr="' + gender + '"] [data-calc-bmr="1"]').val()),
       BMR2                = parseInt($('[data-calc-bmr="' + gender + '"] [data-calc-bmr="2"]').val()),
       BMR3                = parseInt($('[data-calc-bmr="' + gender + '"] [data-calc-bmr="3"]').val()),
       BMR4                = parseInt($('[data-calc-bmr="' + gender + '"] [data-calc-bmr="4"]').val()),
       // Sums
-      sumLBM,
-      sumRecCals,
-      sumRecProt,
-      sumRecProtPercent,
+      sumLBM              = 0,
+      sumRecCals          = 0,
+      sumRecProt          = 0,
+      sumRecProtPercent   = 0,
       //sumRecProtPercentP  = Math.round(((sumRecProt * 4) / sumRecCals) / Math.pow(10, -2) * 10) / 10,
-      sumRecFat,
-      /*sumNeedCarbs        = (weight * workCarbs),
-      sumNeedProt         = (weight * workProt),
-      sumNeedFat          = (weight * workFat),
-      sumNeedWeek1        = (weight * workWeek1),
-      sumNeedWeek2        = (weight * workWeek2),
-      sumNeedWeek3        = (weight * workWeek3),
-      sumNeedWeek4        = (weight * workWeek4),*/
+      sumRecFat           = 0,
       // Macros
-      macroBF,
-      macroCals,
-      macroProt,
-      macroFat,
-      macroCarb,
-      macroCustomCals,
-      macroCustomProt,
-      macroCustomFat,
-      macroCustomCarb;
+      macroBF             = 0,
+      macroCals           = 0,
+      macroProt           = 0,
+      macroFat            = 0,
+      macroCarb           = 0,
+      macroCustomCals     = 0,
+      macroCustomProt     = 0,
+      macroCustomFat      = 0,
+      macroCustomCarb     = 0,
+      macroResultCals     = 0,
+      macroResultProt     = 0,
+      macroResultFat      = 0,
+      macroResultCarb     = 0;
       //macroCarb           = (1 - (sumRecProtPercent - macroBF));
 
-    console.log(formula);
     if (formula === 0) {
-      console.log('formula: 0');
+      $('.input-bodyfat').attr('readonly', 'readonly');
+      $('.input-bodyfat').val('');
       // Athletic people
+      BF                  = 20;
       sumLBM              = (weight * (1 - BF / 100));
       sumRecCals          = Math.floor((12 * sumLBM));
       sumRecProt          = (1.25 * sumLBM);
@@ -73,14 +72,14 @@
       macroFat            = (macroCals * (macroBF / 9));
       macroCarb           = (macroCals * (1 - sumRecProtPercent - macroBF) / 9);
       macroCustomCals     = (activity * (BMR1 + (BMR2 * weight) + (BMR3 * height) - (BMR4 * age)));
-      macroCustomProt     = (macroCals * sumRecProtPercent / 4);
-      macroCustomFat      = (macroCals * (macroBF / 9));
-      macroCustomCarb     = (macroCals * (1 - sumRecProtPercent - macroBF) / 9);
-
+      macroCustomProt     = (macroCustomCals * sumRecProtPercent / 4);
+      macroCustomFat      = (macroCustomCals * (macroBF / 9));
+      macroCustomCarb     = (macroCustomCals * (1 - sumRecProtPercent - macroBF) / 9);
+      console.log('TDDE: ' + sumRecCals);
     }
 
     if (formula === 1) {
-      console.log('formula: 1');
+      $('.input-bodyfat').removeAttr('readonly');
       // Lean/fat people
       sumLBM              = (weight * (1 - BF / 100));
       sumRecCals          = Math.floor((12 * sumLBM));
@@ -89,18 +88,52 @@
       sumRecFat           = 25;
       // Macros
       macroBF             = parseFloat(BF) / 100;
-      macroCals           = (BMR1 + sumRecCals);
+      macroCals           = (sumRecCals);
       macroProt           = (macroCals * sumRecProtPercent / 4);
       macroFat            = (macroCals * (macroBF / 9));
       macroCarb           = (macroCals * (1 - sumRecProtPercent - macroBF) / 9);
-      macroCustomCals     = (activity * (BMR1 + (BMR2 * weight) + (BMR3 * height) - (BMR4 * age)));
-      macroCustomProt     = (macroCals * sumRecProtPercent / 4);
-      macroCustomFat      = (macroCals * (macroBF / 9));
-      macroCustomCarb     = (macroCals * (1 - sumRecProtPercent - macroBF) / 9);
+      macroCustomCals     = (activity * sumRecCals);
+      macroCustomProt     = (macroCustomCals * sumRecProtPercent / 4);
+      macroCustomFat      = (macroCustomCals * (macroBF / 9));
+      macroCustomCarb     = (macroCustomCals * (1 - sumRecProtPercent - macroBF) / 9);
     }
 
-    console.log('calc: ' + calcId, calcVal, macroCals, BMR2 * weight, BMR3 * height, BMR4 * age, activity, activityDesc, gender);
-
+    console.log('calc: ' + calcId, calcVal, goalVal, macroCustomCals);
+    
+    // Goal
+    //updateGoal();
+    function updateGoal($thisGoal, goal, goalCals, macroCustomCals) {
+      if (goal === 'fat-loss') {
+        goalCals = (macroCustomCals - (macroCustomCals * goalVal / 100));
+      } else if (goal === 'maintain') {
+        goalCals = (macroCustomCals);
+      } else {
+        goalCals = (macroCustomCals + (macroCustomCals * goalVal / 100));
+      }
+      $('[data-calc-goal-calories="' + goal + '"]').html(Math.floor(goalCals));
+      if ($thisGoal.is(':checked')) {
+        console.log(goal + ' is checked');
+        goalCalsSelected = goalCals;
+      }
+    }
+    
+    $($goal).each(function(index) {
+      if ($(this).hasClass('selected')) {
+        goal = $(this).data('calc-goal');
+        goalVal = parseInt($(this).attr('value'));
+        var $thisGoal = $(this);
+        updateGoal($thisGoal, goal, goalVal, macroCustomCals);
+      }
+    });
+    
+    // Set Macros (Prob move to step 3 later)
+    macroResultCals     = (goalCalsSelected);
+    macroResultProt     = (macroResultCals * sumRecProtPercent / 4);
+    macroResultFat      = (macroResultCals * (macroBF / 9));
+    macroResultCarb     = (macroResultCals * (1 - sumRecProtPercent - macroBF) / 9);
+    
+    //
+    
     function inputVal(id, sumVal) {
       //sumVal = Math.round(sumVal/100);
       $(id).val(sumVal);
@@ -118,25 +151,26 @@
     inputVal('[data-calc="recProtPercent"]', Math.round(sumRecProtPercent / Math.pow(10, -2) * 10) / 10);
     inputVal('[data-calc="recFat"]', sumRecFat);
 
-    /*// Needs
-    inputVal('[data-calc-need="carbs"]', sumNeedCarbs);
-    inputVal('[data-calc-need="prot"]', sumNeedProt);
-    inputVal('[data-calc-need="fat"]', sumNeedFat);
-    inputVal('[data-calc-need="week1"]', sumNeedWeek1);
-    inputVal('[data-calc-need="week2"]', sumNeedWeek2);
-    inputVal('[data-calc-need="week3"]', sumNeedWeek3);
-    inputVal('[data-calc-need="week4"]', sumNeedWeek4);*/
-
-    // Macros
+    // BMR
     updateVal('[data-calc-level="bmr"] [data-calc-macro="cals"]', Math.floor(macroCals));
     updateVal('[data-calc-level="bmr"] [data-calc-macro="prot"]', Math.floor(macroProt));
     updateVal('[data-calc-level="bmr"] [data-calc-macro="fat"]', Math.floor(macroFat));
     updateVal('[data-calc-level="bmr"] [data-calc-macro="carb"]', Math.floor(macroCarb));
+    // TDDE
     updateVal('[data-calc-level="custom"] [data-calc-macro="level"]', activityDesc);
     updateVal('[data-calc-level="custom"] [data-calc-macro="cals"]', Math.floor(macroCustomCals));
     updateVal('[data-calc-level="custom"] [data-calc-macro="prot"]', Math.floor(macroCustomProt));
     updateVal('[data-calc-level="custom"] [data-calc-macro="fat"]', Math.floor(macroCustomFat));
     updateVal('[data-calc-level="custom"] [data-calc-macro="carb"]', Math.floor(macroCustomCarb));
+    // Result
+    updateVal('[data-calc-level="result"] [data-calc-macro="level"]', activityDesc);
+    updateVal('[data-calc-level="result"] [data-calc-macro="cals"]', Math.floor(macroResultCals));
+    updateVal('[data-calc-level="result"] [data-calc-macro="prot"]', Math.floor(macroResultProt));
+    updateVal('[data-calc-level="result"] [data-calc-macro="fat"]', Math.floor(macroResultFat));
+    updateVal('[data-calc-level="result"] [data-calc-macro="carb"]', Math.floor(macroResultCarb));
+    
+    // Goal
+    //updateVal($goalCals, Math.floor(goalCals));
 
   };
 
--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -13,6 +13,12 @@ $(function () {
     calcResult = new calcUpdate();
     calcId = $(this).data('calc');
     calcVal = $(this).val();
+    if ($(this).data('calc') === 'goal') {
+      $('[data-calc-goal="' + $(this).data('calc-goal') + '"]').removeClass('selected');
+      $('[data-calc-goal="' + $(this).data('calc-goal') + '"]').prop('checked', false);
+      $(this).addClass('selected');
+      $(this).prop('checked', true);
+    }
     calcResult.setValue(calcId, calcVal);
   });
 
