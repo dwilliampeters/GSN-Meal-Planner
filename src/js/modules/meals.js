1b13945a18272edbc181a4618d782d5bbfa6c7c3
SPARTdAN <spartdan@live.co.uk>
2014-12-05 13:27:29 +0000

    SUDE for meals

--- src/js/modules/meals.js
+++ src/js/modules/meals.js
@@ -2,35 +2,18 @@ $(function () {
   "use strict";
 
   // SUDO
-  
+
   // Foods
-  // ID / QTY  / Food    / Grams / Serv.  / Cals / Carbs / Fat / Protein / Sodium / Sugar
-  // 0  / 1    / Banana  / 28    / 1      / 112  / 29    / 0   / 1       / 1      / 15
-  // 1  / 1    / Protein / 28    / 1      / 103  / 3     / 1   / 20      / 191    / 2.5
-  // 2  / 1    / Oats    / 74    / 1      / 74   / 16.5  / 0   / 0       / 0      / 0
-  
-  // Meals
-  // Breakfast / Pre-Workout / Post-Workout / Meal / Bedtime
-  // Breakfast:     0, 1, 2 (Food ID)
-  // Pre-Workout:   0, 1, 2 (Food ID)
-  // Post-Workout:  0, 1, 2 (Food ID)
-  // Meal:          7, 6, 5 (Food ID)
-  // Bedtime:       8, 9    (Food ID)
-  
+  // ID / Meal      / Foods   / Grams     / Cals / Carbs / Fat / Protein / Sodium / Sugar
+  // 0  / Breakfast / Banana  / 1 serving / 112  / 29    / 0   / 1       / 1      / 15
+  // 1  / Breakfast / Protein / 42        / 103  / 3     / 1   / 20      / 191    / 2.5
+  // 2  / Breakfast / Oats    / 30        / 74   / 16.5  / 0   / 0       / 0      / 0
+
   // Macros
   // Calories / Protein / Fat / Carbs
   // 2360     / 236     / 52  / 236
-  
-  // 7 Meals =
-  //    Breakfast
-  //    Meal
-  //    Pre-Workout
-  //    Post-Workout
-  //    Meal
-  //    Meal
-  //    Bedtime
-  
-  // (macroCals / 7) = 337
+
+  // 7 Meals = (macroCals / 7) = 337
   // Breakfast    = 337
   // Meal         = 337
   // Pre-Workout  = 337
@@ -38,8 +21,10 @@ $(function () {
   // Meal         = 337
   // Meal         = 337
   // Bedtime      = 337
-  
-  // Breakfast
-  // 
+
+  // Make a meal
+  // EACH Foods.Meal = Breakfast GET Cals
+  // ADD Cals to near 337
+  // PRINT Foods FOR Breakfast
 
 });
