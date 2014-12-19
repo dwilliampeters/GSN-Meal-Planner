7f4ec15039d8b025d636fb838e0536cb0cd3450e
SPARTdAN <spartdan@live.co.uk>
2014-12-19 15:47:45 +0000

    bit of design, only active step shown.

--- app/js/app.js
+++ app/js/app.js
@@ -658,71 +658,71 @@ $(function () {
 // Steps
 $(function () {
   "use strict";
-  
+
   /*// Step layout
   var stepW     = 0,
       stepFullW = 0;
-  
+
   function stepSize() {
     stepW = ($('.calc-form').width());
     stepFullW = (stepW * 4);
-    
+
     $('.step').width(stepW);
     $('.step-wrap').width(stepFullW);
   }
-  
+
   stepSize();
-  
+
   $('.step-wrap').css('margin-left', -970);*/
-  
+
   var docViewTop  = 0,
       stepsPos    = 0;
-  
+
   $('.steps').height($('.steps').height());
-  
+
   function stepsBar() {
     docViewTop  = $(window).scrollTop(),
     stepsPos    = $('.steps').offset().top;
-    
+
     console.log(docViewTop, stepsPos);
-    
+
     if (docViewTop > stepsPos) {
       $('.steps-bar').addClass('fixed');
     } else {
       $('.steps-bar').removeClass('fixed');
     }
   }
-  
+
   $(window).scroll(function(){
     stepsBar();
     $('.steps').height($('.steps').height());
   })
-  
+
   function stepChange() {
-    
+
   }
-  
+
   var calculate_step = 0;
-  
+
   $('[data-calculate]').on('click', function(e) {
     e.preventDefault();
-    
+
     calculate_step = parseFloat($(this).attr('data-calculate'));
-    
+
     console.log(calculate_step);
     calculate_step = (calculate_step + 1);
     console.log(calculate_step);
-    
+
     $('.steps-step').removeClass('active');
     $('.steps-step.step-' + calculate_step).addClass('active');
-    
-    /*$('.step').removeClass('active');*/
+
+    $('.step').removeClass('active');
     $('.step.step-' + calculate_step).addClass('active');
-    
+
     $('html, body').animate({
       scrollTop: $('.step.step-' + calculate_step).offset().top
     }, 1000);
-    
+
   });
 
 });
